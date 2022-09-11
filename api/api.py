29b52fb8ae32cbf7f3ae314f74993b5ee14c42f9
Valentin <val8673@gmail.com>
2022-09-11 21:59:33 +0500

    Добавил логи но криво

--- api/api.py
+++ api/api.py
@@ -1,3 +1,4 @@
+import logging
 from flask import Blueprint, request, jsonify
 from posts.dao.posts_dao import PostsDao
 
@@ -7,14 +8,19 @@ posts_dao = PostsDao(POSTS_PATH)
 
 api_blueprint = Blueprint('api_blueprint', __name__, template_folder='templates')
 
+logging.basicConfig(filename="api.log", level='INFO')
+logging.Formatter("%(levelname)s : %(asctime)s : %(message)s")
+
 
 @api_blueprint.get('/api/posts')
 def all_posts():
 	posts = posts_dao.load_all()
+	logging.info('Запрос api/posts')
 	return jsonify([post.get_dict() for post in posts]), 200
 
 
 @api_blueprint.get('/api/posts/<int:post_id>')
 def page_post_by_pk(post_id):
 	post = posts_dao.get_post_by_pk(post_id)
+	logging.info(f'Запрос api/posts/{post_id}')
 	return jsonify(post.get_dict()), 200
